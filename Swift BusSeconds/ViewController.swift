8f85866361dfd420ec2c36ede181c835d7a9b9a5
Hiroaki Hirabayashi <hh.38063806@gmail.com>
2021-03-18 14:31:01 +0900

    Initial Commit

---
+++ Swift BusSeconds/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift BusSeconds
+//
+//  Created by 平林 宏淳 on 2021/03/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
